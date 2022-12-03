cbb40b9871f19e37e68e21171df378bd00e25f44
sloumaallagui <56230898+sloumaallagui@users.noreply.github.com>
2022-12-03 21:40:43 +0100

    Angular-mdbootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'solution2';
+  code = `  import  {MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';`
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdbCollapseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
